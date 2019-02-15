a49c508ce31fff141b4dcfa9bb5e4103190c2c01
mooondark <40036185+mooondark@users.noreply.github.com>
2019-02-15 15:29:36 +0100

    Adjust -f option for cut

--- iperf_munin.sh
+++ iperf_munin.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Download speed for 10s with 4 parallels downstream in ipv4
+#Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
+
         down=`iperf3 -f m -c bouygues.iperf.fr -p 5205 -4 -R -P4 -t 10\
                 | grep Mbits \
                 | cut -d ' ' -f 12 \
@@ -8,6 +10,8 @@
         echo "down.value $down" > /etc/munin/iperf.down
 
 #Upload speed for 10s with 3 parallels updtream in ipv4 an omitting the first 2 seconds (-O 2)
+#Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
+
         up=`iperf3 -f m -c bouygues.iperf.fr -p 5205 -4 -P3 -O 2 -t 10\
                 | grep Mbits \
                 | cut -d ' ' -f 13 \
