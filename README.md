f7fac0e75d14fd31cddb07b4b30a8b37579e2b70
mooondark <40036185+mooondark@users.noreply.github.com>
2020-01-14 09:52:34 +0100

    Check if server is busy

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Use this script for monitoring bandwith with iperf3 & munin.
 
 Set a crontab for iperf_munin.sh every 30 mn :
-*/30 * * * *    /usr/bin/iperf_munin.sh
+7,37 * * * *    /usr/bin/iperf_munin.sh > /dev/null 2>&1
+
+![day_exemple screenshot](screenshot/iperf-day.png)
 
 Share & enjoy
--- iperf_munin.sh
+++ iperf_munin.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-#Download speed for 10s with 4 parallels downstream in ipv4
+sleep $[ ( $RANDOM % 20 ) + 15 ]s
+
 #Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
 
-        down=`iperf3 -f m -c bouygues.iperf.fr -p 5205 -4 -R -P4 -t 10\
+        down=`iperf3 -f m -c bouygues.iperf.fr -p 5206 -6 -R -P8 -O 2 -t 8\
                 | grep Mbits \
                 | cut -d ' ' -f 12 \
                 | sed '{$!d;}'`
         echo "down.value $down" > /etc/munin/iperf.down
+        
+#No update if server is busy
+        if [ "$down" = "" ]
+        then
+          exit
+        else
+          echo "down.value $down" > /etc/munin/iperf.down
+        fi
 
-#Upload speed for 10s with 3 parallels updtream in ipv4 an omitting the first 2 seconds (-O 2)
+#Upload speed for 10s with 4 parallels updtream in ipv4 an omitting the first 2 seconds (-O 2)
 #Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
 
-        up=`iperf3 -f m -c bouygues.iperf.fr -p 5205 -4 -P3 -O 2 -t 10\
+        up=`iperf3 -f m -c bouygues.iperf.fr -p 5206 -6 -P4 -O 2 -t 10\
                 | grep Mbits \
                 | cut -d ' ' -f 13 \
                 | sed '{$!d;}'`
         echo "up.value $up" > /etc/munin/iperf.up
+        
+#No update if server is busy
+        if [ "$up" = ""  ]
+        then
+          exit
+        else
+          echo "up.value $up" > /etc/munin/iperf.up
+        fi
