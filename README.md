37b22619ac489fb153b0215f1c82c19a7ab6ecb7
mooondark <40036185+mooondark@users.noreply.github.com>
2026-09-15 22:28:19 +0200

    small fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # munin-iperf
 Use this script for monitoring bandwith with iperf3 & munin.
 
-Set a crontab for iperf_munin.sh every 30 mn :
+Set a crontab for iperf_munin.sh every 30 mn (for exemple):
 7,37 * * * *    /usr/bin/iperf_munin.sh > /dev/null 2>&1
 
 ![day_exemple screenshot](screenshot/iperf-day.png)
--- iperf_munin.sh
+++ iperf_munin.sh
@@ -4,37 +4,33 @@
 # - iperf3.moji.fr : From port 5200 to port 5240
 # - speedtest.milkywan.fr : From port 9200 to port 9240
 
-sleep $[ ( $RANDOM % 20 ) + 15 ]s
+sleep $(( ( RANDOM % 20 ) + 15 ))s
 
 #Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
 
-        down=`iperf3 -f m -c iperf3.moji.fr -p 5217 -6 -R -P8 -O 2 -t 8\
+        down=`timeout 15 iperf3 -f m -c iperf3.moji.fr -p 5217 -6 -R -P8 -O 2 -t 8\
                 | grep Mbits \
                 | cut -d ' ' -f 12 \
                 | sed '{$!d;}'`
-        echo "down.value $down" > /etc/munin/iperf.down
-        
+
 #No update if server is busy
         if [ "$down" = "" ]
         then
           exit
-        else
-          echo "down.value $down" > /etc/munin/iperf.down
         fi
+        echo "down.value $down" > /etc/munin/iperf.down
 
-#Upload speed for 10s with 4 parallels updtream in ipv4 an omitting the first 2 seconds (-O 2)
+#Upload speed for 10s with 4 parallels updtream in ipv6 an omitting the first 2 seconds (-O 2)
 #Ajust the -f 12 option of cut for your version of iperf3 (sometimes it's more or less)
 
-        up=`iperf3 -f m -c iperf3.moji.fr -p 5219 -6 -P4 -O 2 -t 10\
+        up=`timeout 17 iperf3 -f m -c iperf3.moji.fr -p 5219 -6 -P4 -O 2 -t 10\
                 | grep Mbits \
                 | cut -d ' ' -f 13 \
                 | sed '{$!d;}'`
-        echo "up.value $up" > /etc/munin/iperf.up
-        
+
 #No update if server is busy
         if [ "$up" = ""  ]
         then
           exit
-        else
-          echo "up.value $up" > /etc/munin/iperf.up
         fi
+        echo "up.value $up" > /etc/munin/iperf.up
